dbad860afb5d3ccac4ee2508152c6b99737fa2b4
luiscarlosag <luis_carlosss@hotmail.com>
2018-03-30 10:59:27 -0600

    d

--- collection.php
+++ collection.php
@@ -1,3 +1,23 @@
 <?php
 include_once('lib/header.php');
 ?>
+
+
+
+
+
+
+
+
+
+
+
+
+</div>
+</body>
+<script src="js/collection/collection.js"></script>
+<script src="js/collection/init.js"></script>
+<script src="js/collection/dao.js"></script>
+<script src="js/collection/function.js"></script>
+
+</html>
