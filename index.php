79fd4e0c79cbea31950a7d53c7d21134ba1e65f3
luiscarlosag <luis_carlosss@hotmail.com>
2018-03-30 10:47:36 -0600

    c

--- index.php
+++ index.php
@@ -3,12 +3,13 @@ include_once('lib/header.php');
 ?>
 
 <div class="row mx-0">
-<br>
-    <br>
+    <div class="col-12">
 
+
+    </div>
 </div>
-        </div>
-    </body>
+</div>
+</body>
 <script src="js/structure/structure.js"></script>
 <script src="js/structure/init.js"></script>
 <script src="js/structure/dao.js"></script>
