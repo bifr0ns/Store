34cef2a207ae5489025e74f9b6b778377394a386
objkevin <objkevin@gmail.com>
2018-03-31 17:38:41 -0600

    a

--- collection.php
+++ collection.php
@@ -14,6 +14,16 @@ include_once('lib/header.php');
       <li><a class="text-primary" href="#">Fundas silla de paseo</a></li>
     </ul>
   </div>
+  <div class="d-lg-none p-3 col-12 col-sm-9">
+    <select class="custom-select w-100">
+      <option selected>Elegir...</option>
+      <option>Novedades</option>
+      <option>Camisetas</option>
+      <option>Chaquetas</option>
+      <option>Parkas</option>
+      <option>Fundas silla de paseo</option>
+    </select>
+  </div>
   <div class="col-lg-9">
 
     <?php // NOTE: Banner, sólo se debe mostrar en Novedades ?>
@@ -24,7 +34,7 @@ include_once('lib/header.php');
     <div class="row mx-0">
 
       <?php // NOTE: Aquí empieza la carta ?>
-      <div class="col-4 px-4">
+      <div class="col-12 col-sm-6 col-lg-4 px-4 my-4">
         <div class="card">
           <img class="card-img-top" src="./img/recien_nacido_f/camisetas/camiseta_corta_niña_con_lazos/1.jpg" alt="Card image cap">
           <div class="card-body">
@@ -36,26 +46,26 @@ include_once('lib/header.php');
         </div>
       </div>
 
-      <?php // NOTE: Aquí empieza la carta ?>
-      <div class="col-4">
+      <div class="col-12 col-sm-6 col-lg-4 px-4 my-4">
         <div class="card">
-          <img class="card-img-top" src="..." alt="Card image cap">
+          <img class="card-img-top" src="./img/recien_nacido_f/camisetas/camiseta_corta_niña_con_lazos/1.jpg" alt="Card image cap">
           <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+            <p class="card-title text-center">Camiseta corta niña con lazos</p>
+            <p class="card-text text-center">$ 319.00</p>
+            <p class="card-text text-center"><strong>Más colores</strong></p>
+            <p class="mb-0 text-center"><a href="#" class="btn btn-secondary">Añadir a la bolsa</a></p>
           </div>
         </div>
       </div>
 
-      <?php // NOTE: Aquí empieza la carta ?>
-      <div class="col-4">
+      <div class="col-12 col-sm-6 col-lg-4 px-4 my-4">
         <div class="card">
-          <img class="card-img-top" src="..." alt="Card image cap">
+          <img class="card-img-top" src="./img/recien_nacido_f/camisetas/camiseta_corta_niña_con_lazos/1.jpg" alt="Card image cap">
           <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+            <p class="card-title text-center">Camiseta corta niña con lazos</p>
+            <p class="card-text text-center">$ 319.00</p>
+            <p class="card-text text-center"><strong>Más colores</strong></p>
+            <p class="mb-0 text-center"><a href="#" class="btn btn-secondary">Añadir a la bolsa</a></p>
           </div>
         </div>
       </div>
