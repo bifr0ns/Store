1887e30a9a1fc94ef919f5af67d73ee88cee4073
objkevin <objkevin@gmail.com>
2018-04-02 15:04:46 -0500

    a

--- collection.php
+++ collection.php
@@ -15,7 +15,7 @@ include_once('lib/header.php');
     </ul>
   </div>
   <div class="d-lg-none p-3 col-12 col-sm-9">
-    <select class="form-control p-0" style="border: 2px solid #0071ce; color: #0071ce; font-weight: 400;">
+    <select class="form-control p-1" style="border: 2px solid #0071ce; color: #0071ce; font-weight: 400;">
       <option selected disabled>Selecciona...</option>
       <option value="0">Novedades</option>
       <option>Camisetas</option>
@@ -48,11 +48,11 @@ include_once('lib/header.php');
 
 <?php // NOTE: Aquí van los colores de cada articulo ?>
           <div class="w-100 d-inline-flex justify-content-center mb-3">
-            <span class="mr-1 rounded-circle" style="background-color: rgb(229, 222, 90); width: 1.2rem; height: 1.2rem;" data-toggle="tooltip" data-placement="top" title="Limonada"></span>
-            <span class="mr-1 rounded-circle" style="background-color: rgb(91, 117, 230); width: 1.2rem; height: 1.2rem;"></span>
-            <span class="mr-1 rounded-circle" style="background-color: rgb(91, 205, 230); width: 1.2rem; height: 1.2rem;"></span>
-            <span class="mr-1 rounded-circle" style="background-color: rgb(224, 66, 66); width: 1.2rem; height: 1.2rem;"></span>
-            <span class="mr-1 rounded-circle" style="background-color: rgb(230, 91, 199); width: 1.2rem; height: 1.2rem;"></span>
+            <span class="mr-1 rounded-circle" style="background-color: rgb(229, 222, 90); width: 1.2rem; height: 1.2rem;" data-toggle="tooltip" data-placement="bottom" title="Limonada"></span>
+            <span class="mr-1 rounded-circle" style="background-color: rgb(91, 117, 230); width: 1.2rem; height: 1.2rem;" data-toggle="tooltip" data-placement="bottom" title="Limonada"></span>
+            <span class="mr-1 rounded-circle" style="background-color: rgb(91, 205, 230); width: 1.2rem; height: 1.2rem;" data-toggle="tooltip" data-placement="bottom" title="Limonada"></span>
+            <span class="mr-1 rounded-circle" style="background-color: rgb(224, 66, 66); width: 1.2rem; height: 1.2rem;" data-toggle="tooltip" data-placement="bottom" title="Limonada"></span>
+            <span class="mr-1 rounded-circle" style="background-color: rgb(230, 91, 199); width: 1.2rem; height: 1.2rem;" data-toggle="tooltip" data-placement="bottom" title="Limonada"></span>
           </div>
 
 <?php // NOTE:  Boton añadir ?>
@@ -70,11 +70,11 @@ include_once('lib/header.php');
 
 <?php // NOTE: Aquí van los colores de cada articulo ?>
           <div class="w-100 d-inline-flex justify-content-center mb-3">
-            <span class="mr-1 rounded-circle" style="background-color: rgb(229, 222, 90); width: 1.2rem; height: 1.2rem;"></span>
-            <span class="mr-1 rounded-circle" style="background-color: rgb(91, 117, 230); width: 1.2rem; height: 1.2rem;"></span>
-            <span class="mr-1 rounded-circle" style="background-color: rgb(91, 205, 230); width: 1.2rem; height: 1.2rem;"></span>
-            <span class="mr-1 rounded-circle" style="background-color: rgb(224, 66, 66); width: 1.2rem; height: 1.2rem;"></span>
-            <span class="mr-1 rounded-circle" style="background-color: rgb(230, 91, 199); width: 1.2rem; height: 1.2rem;"></span>
+            <span class="mr-1 rounded-circle" style="background-color: rgb(229, 222, 90); width: 1.2rem; height: 1.2rem;" data-toggle="tooltip" data-placement="bottom" title="Limonada"></span>
+            <span class="mr-1 rounded-circle" style="background-color: rgb(91, 117, 230); width: 1.2rem; height: 1.2rem;" data-toggle="tooltip" data-placement="bottom" title="Limonada"></span>
+            <span class="mr-1 rounded-circle" style="background-color: rgb(91, 205, 230); width: 1.2rem; height: 1.2rem;" data-toggle="tooltip" data-placement="bottom" title="Limonada"></span>
+            <span class="mr-1 rounded-circle" style="background-color: rgb(224, 66, 66); width: 1.2rem; height: 1.2rem;" data-toggle="tooltip" data-placement="bottom" title="Limonada"></span>
+            <span class="mr-1 rounded-circle" style="background-color: rgb(230, 91, 199); width: 1.2rem; height: 1.2rem;" data-toggle="tooltip" data-placement="bottom" title="Limonada"></span>
           </div>
 
 <?php // NOTE:  Boton añadir ?>
@@ -92,11 +92,11 @@ include_once('lib/header.php');
 
 <?php // NOTE: Aquí van los colores de cada articulo ?>
           <div class="w-100 d-inline-flex justify-content-center mb-3">
-            <span class="mr-1 rounded-circle" style="background-color: rgb(229, 222, 90); width: 1.2rem; height: 1.2rem;"></span>
-            <span class="mr-1 rounded-circle" style="background-color: rgb(91, 117, 230); width: 1.2rem; height: 1.2rem;"></span>
-            <span class="mr-1 rounded-circle" style="background-color: rgb(91, 205, 230); width: 1.2rem; height: 1.2rem;"></span>
-            <span class="mr-1 rounded-circle" style="background-color: rgb(224, 66, 66); width: 1.2rem; height: 1.2rem;"></span>
-            <span class="mr-1 rounded-circle" style="background-color: rgb(230, 91, 199); width: 1.2rem; height: 1.2rem;"></span>
+            <span class="mr-1 rounded-circle" style="background-color: rgb(229, 222, 90); width: 1.2rem; height: 1.2rem;" data-toggle="tooltip" data-placement="bottom" title="Limonada"></span>
+            <span class="mr-1 rounded-circle" style="background-color: rgb(91, 117, 230); width: 1.2rem; height: 1.2rem;" data-toggle="tooltip" data-placement="bottom" title="Limonada"></span>
+            <span class="mr-1 rounded-circle" style="background-color: rgb(91, 205, 230); width: 1.2rem; height: 1.2rem;" data-toggle="tooltip" data-placement="bottom" title="Limonada"></span>
+            <span class="mr-1 rounded-circle" style="background-color: rgb(224, 66, 66); width: 1.2rem; height: 1.2rem;" data-toggle="tooltip" data-placement="bottom" title="Limonada"></span>
+            <span class="mr-1 rounded-circle" style="background-color: rgb(230, 91, 199); width: 1.2rem; height: 1.2rem;" data-toggle="tooltip" data-placement="bottom" title="Limonada"></span>
           </div>
 
 <?php // NOTE:  Boton añadir ?>
@@ -108,6 +108,7 @@ include_once('lib/header.php');
   </div>
 </div>
 
+<?php // NOTE: Aquí empieza la vista previa del artículo ?>
 <div class="row mx-0">
   <div class="col-2">
     <img class="w-100 px-4 pb-4" src="./img/recien_nacido_f/camisetas/camiseta_corta_niña_con_lazos/1.jpg" alt="">
@@ -128,6 +129,7 @@ include_once('lib/header.php');
     <div class="w-100">
       <br>
       <span>Art . 28-01008-069</span>
+      <b class="float-right" style="font-size: 22px;">$ 319.00</b>
     </div>
     <div class="w-100 mb-3">
       <b>Color: Limonada</b>
@@ -135,19 +137,26 @@ include_once('lib/header.php');
     <div class="w-100 d-inline-flex">
 
       <?php // NOTE: Aquí van los colores de cada articulo ?>
-      <span class="mr-1 rounded-circle" style="background-color: rgb(229, 222, 90); width: 1.2rem; height: 1.2rem;"></span>
-      <span class="mr-1 rounded-circle" style="background-color: rgb(91, 117, 230); width: 1.2rem; height: 1.2rem;"></span>
-      <span class="mr-1 rounded-circle" style="background-color: rgb(91, 205, 230); width: 1.2rem; height: 1.2rem;"></span>
-      <span class="mr-1 rounded-circle" style="background-color: rgb(224, 66, 66); width: 1.2rem; height: 1.2rem;"></span>
-      <span class="mr-1 rounded-circle" style="background-color: rgb(230, 91, 199); width: 1.2rem; height: 1.2rem;"></span>
+      <span class="mr-1 rounded-circle" style="background-color: rgb(229, 222, 90); width: 1.2rem; height: 1.2rem;" data-toggle="tooltip" data-placement="bottom" title="Limonada"></span>
+      <span class="mr-1 rounded-circle" style="background-color: rgb(91, 117, 230); width: 1.2rem; height: 1.2rem;" data-toggle="tooltip" data-placement="bottom" title="Limonada"></span>
+      <span class="mr-1 rounded-circle" style="background-color: rgb(91, 205, 230); width: 1.2rem; height: 1.2rem;" data-toggle="tooltip" data-placement="bottom" title="Limonada"></span>
+      <span class="mr-1 rounded-circle" style="background-color: rgb(224, 66, 66); width: 1.2rem; height: 1.2rem;" data-toggle="tooltip" data-placement="bottom" title="Limonada"></span>
+      <span class="mr-1 rounded-circle" style="background-color: rgb(230, 91, 199); width: 1.2rem; height: 1.2rem;" data-toggle="tooltip" data-placement="bottom" title="Limonada"></span>
 
     </div>
     <br>
     <br>
     <br>
-    <br>
+
+    <div class="w-100">
+      <div class="float-right" style="color: #0071ce; font-size: 17px;">
+        <span class="fa fa-tag"></span>
+        <a><strong>Guía de Tallas</strong></a>
+      </div>
+    </div>
+    <?php // NOTE: el select de las tallas ?>
     <div class="w-100 mb-3">
-      <select class="form-control p-0" style="border: 2px solid #0071ce; color: #0071ce; font-weight: 400;">
+      <select class="form-control p-1" style="border: 2px solid #0071ce; color: #0071ce; font-weight: 400;">
         <option selected disabled>Selecciona tu talla...</option>
         <option value="0">0-1 Meses</option>
         <option>1-2 Meses</option>
@@ -158,6 +167,28 @@ include_once('lib/header.php');
       </select>
     </div>
     <p class="text-center"><a href="#" class="btn btn-secondary px-0 text-center w-100" style="font-size:0.8rem; background-color: #0071ce;"><b>AÑADIR A LA BOLSA</b></a></p>
+
+    <?php // NOTE: la descripción del articulo ?>
+    <div class="text-center w-100">
+      <ul class="nav nav-tabs card-header-tabs mx-0">
+        <li class="nav-item">
+          <a class="nav-link active"><b>Descripción</b></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link"><b>Composición</b></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link"><b>Envío</b></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link"><b>Devoluciones</b></a>
+        </li>
+      </ul>
+      <div class="card-body">
+        <p class="text-justify" style="font-weight: 400;">Camiseta corta de verano para niña, diseño sencillo con cuello canalé destacando bonitas mangas de tipo ranglan, acabados en volante con terminación richie. Parte baja adornada con bonitos lazos de patrón finalizando en volantes, prenda fresca y elegante.</p>
+      </div>
+    </div>
+
   </div>
 </div>
 
