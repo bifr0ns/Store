50576508d7517ed4d3eb1b4e5bbe1a42f85a3183
objkevin <objkevin@gmail.com>
2018-04-01 21:52:52 -0500

    x

--- collection.php
+++ collection.php
@@ -5,7 +5,7 @@ include_once('lib/header.php');
 <div class="row mx-0">
   <div class="col-lg-3 d-none d-lg-block">
     <ul class="list-unstyled ml-3">
-      <strong style="font-size: 17px; margin-left: -9px;">Colección</strong>
+      <strong style="font-size: 17px; margin-left: -9px;">COLECCIÓN</strong>
       <li><a class="text-primary" href="#" style="font-weight: 400; color: #0072CE">Novedades</a></li>
       <li><a class="text-primary" href="#" style="font-weight: 400; color: #0072CE">Camisetas</a></li>
       <li><a class="text-primary" href="#" style="font-weight: 400; color: #0072CE">Rebecas</a></li>
